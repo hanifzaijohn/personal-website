607f75715ab41fc89eabff85884ec939903ee8c1
Johnny <hanifzaij@gmail.com>
2019-07-02 20:59:14 -0700

    index html & javascript file

---
+++ app.js
@@ -0,0 +1,65 @@
+customElements.define('modal-content', class ModalContent extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+        <ion-header translucent>
+          <ion-toolbar>
+            <ion-title>About Me</ion-title>
+            <ion-buttons slot="end">
+              <ion-button onclick="dismissModal()">Close</ion-button>
+            </ion-buttons>
+          </ion-toolbar>
+        </ion-header>
+        <ion-content fullscreen>
+          <ion-list>
+            <ion-item>
+              <ion-label>
+                <h2>I am a Software Dev</h2>
+                <ion-button>
+                <ion-icon slot="icon-only" name="contact"></ion-icon>
+              </ion-button>
+                <p>Web Development & Augmented/Virtual reality. <br> I like making stuff that draws people in.</p>
+              </ion-label>
+            </ion-item>
+            <ion-item>
+              <ion-label>
+                <h2>I like:</h2>
+									<ul>
+										<li> Music (I really listen to everything) </li>
+										<p> Currently on a goth rock and trap vibe.
+										<br>
+										<br>
+										<li> Food (I will try anything) </li>
+										<br>
+										<li> Vidya Games (Video Games) </li>
+										<p> Honestly I just like fighting Games. League is cool too. </p>
+										<br>
+									</ul>
+              </ion-label>
+          </ion-list>
+        </ion-content>
+      `;
+    }
+  });
+  let currentModal = null;
+  const controller = document.querySelector('#abt-me-controller');
+  const button = document.querySelector("#about-btn");
+  if(button) button.addEventListener('click', createModal);
+  else console.log('Modal button NOT WORKING!!!!');
+
+  if(!controller) console.log('Controller is not working');
+  
+  function createModal() {
+    controller.create({
+      component: 'modal-content'
+    }).then(modal => {
+        console.log('does this work?');
+      modal.present();
+      currentModal = modal;
+    });
+  }
+  
+  function dismissModal() {
+    if (currentModal) {
+      currentModal.dismiss().then(() => { currentModal = null; });
+    }
+  }
